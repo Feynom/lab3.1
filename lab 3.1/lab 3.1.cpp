bf2795fab5756449bdf14bce57c38a2eec7b4eef
Feynom <chreptak12@gmail.com>
2020-10-10 09:55:37 +0300

    lab3.1

--- lab 3.1/lab 3.1.cpp
+++ lab 3.1/lab 3.1.cpp
@@ -15,23 +15,23 @@ int main()
     cout << "Введіть значення х:";
     cin >> x;
     if (x < 4)
-        b = 3 * pow(x, 5) - pow(x, 3) + 2 * x - 1;
+        b = 3 * pow(x, 5.) - pow(x, 3.) + 2 * x - 1;
     if (4 <= x && x < 7)
-        b = atan((x - 2) / 3);
+        b = atan((x - 2) / 3.);
     if (x >= 7)
-        b = log10(2 * pow(x, -1) + exp(3*x + 1));
+        b = log10(2 * pow(x, -1.) + exp(3*x + 1));
     a = abs(9 * x * x * x + 2);
     y = a + b;
     cout << "\n1)Значення у = " << y << endl;
 
     if (x<4)
-        b = 3 * pow(x, 5) - pow(x, 3) + 2 * x - 1;
+        b = 3 * pow(x, 5.) - pow(x, 3.) + 2 * x - 1;
     else
         if (4 <= x && x < 7)
-            b = atan((x - 2) / 3);
+            b = atan((x - 2) / 3.);
         else
             if (x >= 7)
-                b = log10(2 * pow(x, -1) + exp(3 * x + 1));
+                b = log10(2 * pow(x, -1.) + exp(3 * x + 1));
     y = a + b;
     cout << "\n2)Значення у = " << y << endl;  
 
